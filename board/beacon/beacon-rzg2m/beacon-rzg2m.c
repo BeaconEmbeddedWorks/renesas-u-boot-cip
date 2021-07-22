2f5dcf6b9d1a69676bb67a22299b309b191cad73
Charles Stevens <charles.stevens@compasses.com>
2021-07-22 17:56:09 -0500

    RSOM-313 - USB host support OTG port
    Port some code from HiHope board file to put the HSUSB module in host
    only mode.
    
    Signed-off-by: Charles Stevens <charles.stevens@compasses.com>

--- board/beacon/beacon-rzg2m/beacon-rzg2m.c
+++ board/beacon/beacon-rzg2m/beacon-rzg2m.c
@@ -7,6 +7,7 @@
 #include <asm/global_data.h>
 #include <asm/io.h>
 #include <asm/arch/rcar-mstp.h>
+#include <linux/delay.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -20,10 +21,32 @@ int board_early_init_f(void)
 	return 0;
 }
 
+/* HSUSB block registers */
+#define HSUSB_REG_LPSTS			0xE6590102
+#define HSUSB_REG_LPSTS_SUSPM_NORMAL	BIT(14)
+#define HSUSB_REG_UGCTRL2		0xE6590184
+#define HSUSB_REG_UGCTRL2_USB0SEL	0x30
+#define HSUSB_REG_UGCTRL2_USB0SEL_EHCI	0x10
+#define HSUSB_REG_UGCTRL2_RESERVED_3	0x00000001 /* bit[3:0] must be B'0001 */
+#define HSUSB_MSTP704		BIT(4)	/* HSUSB */
+
 int board_init(void)
 {
 	/* address of boot parameters */
 	gd->bd->bi_boot_params = CONFIG_SYS_TEXT_BASE + 0x50000;
+
+	/* USB1 pull-up */
+	setbits_le32(PFC_PUEN6, PUEN_USB1_OVC | PUEN_USB1_PWEN);
+
+	/* Configure the HSUSB block */
+	mstp_clrbits_le32(SMSTPCR7, SMSTPCR7, HSUSB_MSTP704);
+	udelay(2);
+	/* Choice USB0SEL */
+	writel(HSUSB_REG_UGCTRL2_USB0SEL_EHCI | HSUSB_REG_UGCTRL2_RESERVED_3,
+	       HSUSB_REG_UGCTRL2);
+	/* low power status */
+	setbits_le16(HSUSB_REG_LPSTS, HSUSB_REG_LPSTS_SUSPM_NORMAL);
+
 
 	return 0;
 }
